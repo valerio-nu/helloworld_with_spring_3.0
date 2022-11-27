509c1c27036fe910657a962803b6790686742f61
Henry <henrichukwudi@gmail.com>
2022-11-28 00:47:53 +0100

    initial project push

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # helloworld_with_spring_3.0
+This application was developed to test the new Spring Boot 3.0 launched recently.
---
+++ src/main/java/com/valerio/hello_world/HelloWorldApplication.java
@@ -0,0 +1,15 @@
+package com.valerio.hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+
+}
